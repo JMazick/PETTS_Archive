280fcacb58ba18c1535bc3fcdf03f2d87b8f55cd
Ben Salinas <ben@luminairecoffee.com>
2022-03-22 20:40:36 -0500

    Code for doing the sim stage definitions

--- PETTSDoll.js
+++ PETTSDoll.js
@@ -3,6 +3,24 @@ let PETTS_NOTIFY = "beb5483e-36e1-4688-b7f5-ea07361b26a9";
 let PETTS_WRITE = "beb5483e-36e1-4688-b7f5-ea07361b26aa";
 let PETTS_LUNG_PRESSURE = "beb5483e-36e1-4688-b7f5-ea07361b26ab";
 let PETTS_COLOR = "beb5483e-36e1-4688-b7f5-ea07361b26ac";
+let PETTS_SIM_1 = "beb5483e-36e1-4688-b7f5-ea07361b26ad"
+let PETTS_SIM_2 = "beb5483e-36e1-4688-b7f5-ea07361b26ae"
+let PETTS_SIM_3 = "beb5483e-36e1-4688-b7f5-ea07361b26af"
+let PETTS_SIM_4 = "beb5483e-36e1-4688-b7f5-ea07361b26ba"
+let PETTS_SIM_5 = "beb5483e-36e1-4688-b7f5-ea07361b26bb"
+let PETTS_SIM_6 = "beb5483e-36e1-4688-b7f5-ea07361b26bc"
+let PETTS_SIM_7 = "beb5483e-36e1-4688-b7f5-ea07361b26bd"
+let PETTS_SIM_8 = "beb5483e-36e1-4688-b7f5-ea07361b26be"
+
+let PETTS_STAGE_STABLE = 0
+let PETTS_STAGE_DISTRESS = 1
+let PETTS_STAGE_ARREST1 = 2
+let PETTS_STAGE_ARREST2 = 3
+let PETTS_STAGE_ARREST3 = 4
+let PETTS_STAGE_RECOVERY = 5
+let PETTS_STAGE_FAILURE = 6
+let PETTS_STAGE_SUCCESS = 7
+const sim_def_lookup = [PETTS_SIM_1, PETTS_SIM_2,PETTS_SIM_3,PETTS_SIM_4, PETTS_SIM_5, PETTS_SIM_6, PETTS_SIM_7, PETTS_SIM_8];
 const sim_state_word_lookup = ["Stable","Distress","Arrest1","Arrest2","Arrest3","Recovery","Failure","Success"]
 class PETTSDoll {
 
@@ -108,6 +126,71 @@ class PETTSDoll {
     return this.device.gatt.getPrimaryService(PETTS_SERVICE)
     .then(service => service.getCharacteristic(PETTS_COLOR))
     .then(characteristic =>characteristic.writeValue(data))
+  }
+  readSimDef(stage)
+  {
+    if(stage < 0 || stage > PETTS_STAGE_SUCCESS) return false;
+
+    return this.device.gatt.getPrimaryService(PETTS_SERVICE)
+    .then(service => service.getCharacteristic(sim_def_lookup[stage]))
+    .then(characteristic => characteristic.readValue())
+    .then((value)=>{
+        let view32 = new Uint32Array(value.buffer.slice(0,8));
+        let view8 = new Uint8Array(value.buffer);
+        let breath_duration_ms = view32[0];
+        let duration_ms = view32[1];
+        let index = 8;//4+4 for the two uint32 values
+        let colors = {
+            "head":{"red":view8[index++], "green":view8[index++], "blue":view8[index++],"white":view8[index++],"brightness":view8[index++]},
+            "arms":{"red":view8[index++], "green":view8[index++], "blue":view8[index++],"white":view8[index++],"brightness":view8[index++]},
+            "torso":{"red":view8[index++], "green":view8[index++], "blue":view8[index++],"white":view8[index++],"brightness":view8[index++]}
+        };
+        return {"breath_duration_ms":breath_duration_ms, "duration_ms":duration_ms, "colors":colors};
+    })
+  }
+  writeSimDef(stage, new_values)
+  {
+    if(stage < 0 || stage > PETTS_STAGE_SUCCESS) return false;
+        let view8 = new Uint8Array(4+4+5*3);
+        let view32 = new Uint32Array(2)
+        // let view8 = new Uint8Array(buffer);
+        view32[0] = new_values["breath_duration_ms"];
+        view32[1] = new_values["duration_ms"];
+        let temp8 = new Uint8Array(view32.buffer);
+        for(let i=0; i<8; i++)
+        {
+            view8[i] = temp8[i];
+        }
+        let index = 8;
+        let color = new_values["colors"]["head"];
+        view8[index++] = color["red"];
+        view8[index++] = color["green"];
+        view8[index++] = color["blue"];
+        view8[index++] = color["white"];
+        view8[index++] = color["brightness"];
+
+        color = new_values["colors"]["arms"];
+        view8[index++] = color["red"];
+        view8[index++] = color["green"];
+        view8[index++] = color["blue"];
+        view8[index++] = color["white"];
+        view8[index++] = color["brightness"];
+
+        color = new_values["colors"]["torso"];
+        view8[index++] = color["red"];
+        view8[index++] = color["green"];
+        view8[index++] = color["blue"];
+        view8[index++] = color["white"];
+        view8[index++] = color["brightness"];
+
+        for(var i=0; i<23; i++)
+        {
+            console.log("\tdata["+i+"]="+view8[i]);
+        }
+
+    return this.device.gatt.getPrimaryService(PETTS_SERVICE)
+    .then(service => service.getCharacteristic(sim_def_lookup[stage]))
+    .then(characteristic => characteristic.writeValue(view8))
   }
 
     unpack_notify_data(buffer)
